b0e2478b8d11b8b2ba1f99717b47df5d1fe11701
jascas93 <jascas_14@hotmail.com>
2022-07-01 13:48:06 -0400

    u

--- jira-issues-tasks/er-3398/js-files/review-and-update.js
+++ jira-issues-tasks/er-3398/js-files/review-and-update.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
         if (window.location.href.indexOf("?welcome=1") > 0) {
             $("#steps_normal_row").addClass("d-none");
             $("#page_titles_row").addClass("mt-4");
+            $("#main_page_title").html("Modify Your Reservation");
         }
     },10);
 });
