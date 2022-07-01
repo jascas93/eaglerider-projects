1555d29f58ec61128b1e573c9d65bbe27d456efa
jascas93 <jascas_14@hotmail.com>
2022-07-01 14:57:53 -0400

    big js change

--- jira-issues-tasks/er-3398/js-files/add-extras.js
+++ jira-issues-tasks/er-3398/js-files/add-extras.js
@@ -26,16 +26,47 @@ $(document).ready(function () {
 });
 
 function checkbox_changed(e) {
-    e.checked ? document.getElementById("add_vip_zero").classList.remove("input-focus-on-load") : document.getElementById("add_vip_zero").classList.add("input-focus-on-load"),
-    e.checked ? document.getElementById("continue_to_review_btn_top").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html") : document.getElementById("continue_to_review_btn_top").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html"),
-    e.checked ? document.getElementById("continue_to_review_btn_bottom").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html") : document.getElementById("continue_to_review_btn_bottom").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html"),
-    e.checked ? document.getElementById("footer_continue").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html") : document.getElementById("footer_continue").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html"),
-    e.checked ? (document.getElementById("balance_value").innerHTML = "$35") : (document.getElementById("balance_value").innerHTML = "$0"),
-    e.checked ? (document.getElementById("balance_value_top").innerHTML = "$35") : (document.getElementById("balance_value_top").innerHTML = "$0"),
-    e.checked ? (document.getElementById("balance_value_bottom").innerHTML = "$35") : (document.getElementById("balance_value_bottom").innerHTML = "$0"),
-    e.checked ? (document.getElementById("total_value").innerHTML = "$243.97") : (document.getElementById("total_value").innerHTML = "$208.97"),
-    e.checked ? document.getElementById("vip_zero_td").classList.remove("d-none") : document.getElementById("vip_zero_td").classList.add("d-none"),
-    e.checked ? document.getElementById("continue_to_review_btn_top").classList.add("btn--shockwave", "is-active") : document.getElementById("continue_to_review_btn_top").classList.remove("btn--shockwave", "is-active"),
-    e.checked ? document.getElementById("continue_to_review_btn_bottom").classList.add("btn--shockwave", "is-active") : document.getElementById("continue_to_review_btn_bottom").classList.remove("btn--shockwave", "is-active"),
-    e.checked ? document.getElementById("footer_continue").classList.add("btn--shockwave", "is-active") : document.getElementById("footer_continue").classList.remove("btn--shockwave", "is-active");
+    if (e.checked) {
+        document.getElementById("add_vip_zero").classList.remove("input-focus-on-load");
+        document.getElementById("continue_to_review_btn_top").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html");
+        document.getElementById("continue_to_review_btn_bottom").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html");
+        document.getElementById("footer_continue").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html");
+        (document.getElementById("balance_value").innerHTML = "$35");
+        (document.getElementById("balance_value_top").innerHTML = "$35");
+        (document.getElementById("balance_value_bottom").innerHTML = "$35");
+        (document.getElementById("total_value").innerHTML = "$243.97");
+        document.getElementById("vip_zero_td").classList.remove("d-none");
+        document.getElementById("continue_to_review_btn_top").classList.add("btn--shockwave", "is-active");
+        document.getElementById("continue_to_review_btn_bottom").classList.add("btn--shockwave", "is-active");
+        document.getElementById("footer_continue").classList.add("btn--shockwave", "is-active");
+    }
+    else {
+        document.getElementById("add_vip_zero").classList.add("input-focus-on-load");
+        document.getElementById("continue_to_review_btn_top").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html");
+        document.getElementById("continue_to_review_btn_bottom").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html");
+        document.getElementById("footer_continue").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html");
+        (document.getElementById("balance_value").innerHTML = "$0");
+        (document.getElementById("balance_value_top").innerHTML = "$0");
+        (document.getElementById("balance_value_bottom").innerHTML = "$0");
+        (document.getElementById("total_value").innerHTML = "$208.97");
+        document.getElementById("vip_zero_td").classList.add("d-none");
+        document.getElementById("continue_to_review_btn_top").classList.remove("btn--shockwave", "is-active");
+        document.getElementById("continue_to_review_btn_bottom").classList.remove("btn--shockwave", "is-active");
+        document.getElementById("footer_continue").classList.remove("btn--shockwave", "is-active");
+    }
 }
+
+//function checkbox_changed(e) {
+//    e.checked ? document.getElementById("add_vip_zero").classList.remove("input-focus-on-load") : document.getElementById("add_vip_zero").classList.add("input-focus-on-load"),
+//    e.checked ? document.getElementById("continue_to_review_btn_top").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html") : document.getElementById("continue_to_review_btn_top").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html"),
+//    e.checked ? document.getElementById("continue_to_review_btn_bottom").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html") : document.getElementById("continue_to_review_btn_bottom").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html"),
+//    e.checked ? document.getElementById("footer_continue").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-added-vip-zero.html") : document.getElementById("footer_continue").setAttribute("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/mockups/new/modify-reservation-review-and-update-no-change.html"),
+//    e.checked ? (document.getElementById("balance_value").innerHTML = "$35") : (document.getElementById("balance_value").innerHTML = "$0"),
+//    e.checked ? (document.getElementById("balance_value_top").innerHTML = "$35") : (document.getElementById("balance_value_top").innerHTML = "$0"),
+//    e.checked ? (document.getElementById("balance_value_bottom").innerHTML = "$35") : (document.getElementById("balance_value_bottom").innerHTML = "$0"),
+//    e.checked ? (document.getElementById("total_value").innerHTML = "$243.97") : (document.getElementById("total_value").innerHTML = "$208.97"),
+//    e.checked ? document.getElementById("vip_zero_td").classList.remove("d-none") : document.getElementById("vip_zero_td").classList.add("d-none"),
+//    e.checked ? document.getElementById("continue_to_review_btn_top").classList.add("btn--shockwave", "is-active") : document.getElementById("continue_to_review_btn_top").classList.remove("btn--shockwave", "is-active"),
+//    e.checked ? document.getElementById("continue_to_review_btn_bottom").classList.add("btn--shockwave", "is-active") : document.getElementById("continue_to_review_btn_bottom").classList.remove("btn--shockwave", "is-active"),
+//    e.checked ? document.getElementById("footer_continue").classList.add("btn--shockwave", "is-active") : document.getElementById("footer_continue").classList.remove("btn--shockwave", "is-active");
+//}
