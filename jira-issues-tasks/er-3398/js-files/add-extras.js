2b8b7623675e5c53adc627f6b86909676d437d15
jascas93 <jascas_14@hotmail.com>
2022-07-04 13:59:47 -0400

    u

--- jira-issues-tasks/er-3398/js-files/add-extras.js
+++ jira-issues-tasks/er-3398/js-files/add-extras.js
@@ -19,6 +19,15 @@ function addVIPZero() {
     // If the checkbox is checked, display the output text
     if (checkBox.checked == true && window.location.href.indexOf("?upsell=1") > 0) {
         $("#vip_zero_line_item").removeClass("d-none");
+        $("#vip_zero_line_item").addClass("line-item-updated");
+        $("#continue_to_review_btn_top").addClass("btn--shockwave", "is-active");
+        $("#continue_to_review_btn_bottom").addClass("btn--shockwave", "is-active");
+        $("#footer_continue").addClass("btn--shockwave", "is-active");
+        $("#total_value").html("$$243.97");
+        $("#paid_value").html("($208.97)");
+        $("#balance_value").html("$35");
+        $("#balance_value_top").html("$35");
+        $("#balance_value_bottom").html("$35");
     }
     else if (checkBox.checked == false && window.location.href.indexOf("?upsell=1") > 0) {
         $("#vip_zero_line_item").addClass("d-none");
