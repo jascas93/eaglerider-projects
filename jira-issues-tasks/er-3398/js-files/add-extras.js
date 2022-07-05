1ee54c26b0ac7a0fd3557616a950060f0dd92e5d
jascas93 <jascas_14@hotmail.com>
2022-07-05 17:23:49 -0400

    u

--- jira-issues-tasks/er-3398/js-files/add-extras.js
+++ jira-issues-tasks/er-3398/js-files/add-extras.js
@@ -1,10 +1,10 @@
 // Page Loader
 $(window).on("load", function () {
     setTimeout(removeLoader, 800); //wait x ms
-    if (window.location.href.indexOf("upsell=1") > 0 && window.location.href.indexOf("model_upgrade=1") < 1) {
+    if (window.location.href.indexOf("upsell_added=1") > 0 && window.location.href.indexOf("model_upgrade=1") < 1) {
         $("#add_vip_zero").trigger("click");
     }
-    else if (window.location.href.indexOf("upsell=1") < 1 && window.location.href.indexOf("model_upgrade=1") > 0) {
+    else if (window.location.href.indexOf("upsell_added=1") < 1 && window.location.href.indexOf("model_upgrade=1") > 0) {
         $("#current_bike_model_img").attr("src", "https://raw.githubusercontent.com/jascas93/eaglerider-projects/main/jira-issues-tasks/er-3398/assets/hd-tri-glide-for-rent.jpeg");
         $("#current_bike_model_name").html("Harley-Davidson® Tri Glide® Ultra");
         $("#old_model_textfield").removeClass("d-none");
@@ -29,7 +29,7 @@ $(window).on("load", function () {
         $("#continue_to_review_btn_bottom").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
         $("#footer_continue").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
     }
-    else if (window.location.href.indexOf("upsell=1") > 0 && window.location.href.indexOf("model_upgrade=1") > 0) {
+    else if (window.location.href.indexOf("upsell_added=1") > 0 && window.location.href.indexOf("model_upgrade=1") > 0) {
         $("#add_vip_zero").trigger("click");
     }
 });
@@ -43,7 +43,7 @@ function removeLoader() {
 function addVIPZero() {
     var checkBox = document.getElementById("add_vip_zero");
 
-    if (checkBox.checked == true && window.location.href.indexOf("upsell=1") > 0 && window.location.href.indexOf("model_upgrade=1") < 1) {
+    if (checkBox.checked == true && window.location.href.indexOf("upsell_added=1") > 0 && window.location.href.indexOf("model_upgrade=1") < 1) {
         $("#total_value").html("$243.97");
         $("#paid_value").html("($208.97)");
         $("#balance_value").html("$35");
@@ -58,7 +58,7 @@ function addVIPZero() {
         $("#continue_to_review_btn_bottom").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update?upsell_added=1.html");
         $("#footer_continue").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update?upsell_added=1.html");
     }
-    else if (checkBox.checked == true && window.location.href.indexOf("upsell=1") < 1 && window.location.href.indexOf("model_upgrade=1") < 1) {
+    else if (checkBox.checked == true && window.location.href.indexOf("upsell_added=1") < 1 && window.location.href.indexOf("model_upgrade=1") < 1) {
         $("#total_value").html("$243.97");
         $("#paid_value").html("($208.97)");
         $("#balance_value").html("$35");
@@ -73,7 +73,22 @@ function addVIPZero() {
         $("#continue_to_review_btn_bottom").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update?upsell_added=1.html");
         $("#footer_continue").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update?upsell_added=1.html");
     }
-    else if (checkBox.checked == false && window.location.href.indexOf("upsell=1") > 0 && window.location.href.indexOf("model_upgrade=1") < 1) {
+    else if (checkBox.checked == false && window.location.href.indexOf("upsell_added=1") < 1 && window.location.href.indexOf("model_upgrade=1") < 1) {
+        $("#total_value").html("$208.97");
+        $("#paid_value").html("($208.97)");
+        $("#balance_value").html("$0");
+        $("#balance_value_top").html("$0");
+        $("#balance_value_bottom").html("$0");
+        $("#vip_zero_line_item").addClass("d-none");
+        $("#vip_zero_line_item").removeClass("line-item-updated");
+        $("#continue_to_review_btn_top").removeClass("btn--shockwave is-active");
+        $("#continue_to_review_btn_bottom").removeClass("btn--shockwave is-active");
+        $("#footer_continue").removeClass("btn--shockwave is-active");
+        $("#continue_to_review_btn_top").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
+        $("#continue_to_review_btn_bottom").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
+        $("#footer_continue").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
+    }
+    else if (checkBox.checked == false && window.location.href.indexOf("upsell_added=1") > 0 && window.location.href.indexOf("model_upgrade=1") < 1) {
         $("#total_value").html("$208.97");
         $("#paid_value").html("($208.97)");
         $("#balance_value").html("$0");
@@ -88,7 +103,7 @@ function addVIPZero() {
         $("#continue_to_review_btn_bottom").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
         $("#footer_continue").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
     }
-    else if (checkBox.checked == true && window.location.href.indexOf("upsell=1") < 1 && window.location.href.indexOf("model_upgrade=1") > 0) {
+    else if (checkBox.checked == true && window.location.href.indexOf("upsell_added=1") < 1 && window.location.href.indexOf("model_upgrade=1") > 0) {
         $("#current_bike_model_img").attr("src", "https://raw.githubusercontent.com/jascas93/eaglerider-projects/main/jira-issues-tasks/er-3398/assets/hd-tri-glide-for-rent.jpeg");
         $("#current_bike_model_name").html("Harley-Davidson® Tri Glide® Ultra");
         $("#old_model_textfield").removeClass("d-none");
@@ -113,7 +128,7 @@ function addVIPZero() {
         $("#continue_to_review_btn_bottom").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
         $("#footer_continue").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
     }
-    else if (checkBox.checked == false && window.location.href.indexOf("upsell=1") < 1 && window.location.href.indexOf("model_upgrade=1") > 0) {
+    else if (checkBox.checked == false && window.location.href.indexOf("upsell_added=1") < 1 && window.location.href.indexOf("model_upgrade=1") > 0) {
         $("#current_bike_model_img").attr("src", "https://raw.githubusercontent.com/jascas93/eaglerider-projects/main/jira-issues-tasks/er-3398/assets/hd-tri-glide-for-rent.jpeg");
         $("#current_bike_model_name").html("Harley-Davidson® Tri Glide® Ultra");
         $("#old_model_textfield").removeClass("d-none");
@@ -138,7 +153,7 @@ function addVIPZero() {
         $("#continue_to_review_btn_bottom").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
         $("#footer_continue").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
     }
-    else if (checkBox.checked == true && window.location.href.indexOf("upsell=1") > 0 && window.location.href.indexOf("model_upgrade=1") > 0) {
+    else if (checkBox.checked == true && window.location.href.indexOf("upsell_added=1") > 0 && window.location.href.indexOf("model_upgrade=1") > 0) {
         $("#current_bike_model_img").attr("src", "https://raw.githubusercontent.com/jascas93/eaglerider-projects/main/jira-issues-tasks/er-3398/assets/hd-tri-glide-for-rent.jpeg");
         $("#current_bike_model_name").html("Harley-Davidson® Tri Glide® Ultra");
         $("#old_model_textfield").removeClass("d-none");
@@ -163,7 +178,7 @@ function addVIPZero() {
         $("#continue_to_review_btn_bottom").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
         $("#footer_continue").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html");
     }
-    else if (checkBox.checked == false && window.location.href.indexOf("upsell=1") > 0 && window.location.href.indexOf("model_upgrade=1") > 0) {
+    else if (checkBox.checked == false && window.location.href.indexOf("upsell_added=1") > 0 && window.location.href.indexOf("model_upgrade=1") > 0) {
         $("#current_bike_model_img").attr("src", "https://raw.githubusercontent.com/jascas93/eaglerider-projects/main/jira-issues-tasks/er-3398/assets/hd-tri-glide-for-rent.jpeg");
         $("#current_bike_model_name").html("Harley-Davidson® Tri Glide® Ultra");
         $("#old_model_textfield").removeClass("d-none");
