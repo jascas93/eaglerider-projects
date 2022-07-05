cb4bb7ff5af1a3cf8dbb5107a8826f7a701a5af9
jascas93 <jascas_14@hotmail.com>
2022-07-05 11:05:20 -0400

    u

--- jira-issues-tasks/er-3398/js-files/review-and-update.js
+++ jira-issues-tasks/er-3398/js-files/review-and-update.js
@@ -21,19 +21,24 @@ $(document).ready(function () {
             $("#main_page_title").html("Modify Your Reservation");
         }
         else if (window.location.href.indexOf("added_upsell=1") > 0 && window.location.href.indexOf("model_upgrade=1") < 1) {
-            $("#total_value").html("$243.97");
-            $("#paid_value").html("($208.97)");
-            $("#balance_value").html("$35");
-            $("#balance_value_top").html("$35");
-            $("#balance_value_bottom").html("$35");
-            $("#vip_zero_line_item").removeClass("d-none");
-            $("#vip_zero_line_item").addClass("line-item-updated");
+            $("#total_value_1").html("$243.97");
+            $("#paid_value_1").html("($208.97)");
+            $("#balance_value_1").html("$35");
+            $("#vip_zero_line_item_1").removeClass("d-none");
+            $("#vip_zero_line_item_1").addClass("line-item-updated");
+            $("#total_value_2").html("$243.97");
+            $("#paid_value_2").html("($208.97)");
+            $("#balance_value_2").html("$35");
+            $("#vip_zero_line_item_2").removeClass("d-none");
+            $("#vip_zero_line_item_2").addClass("line-item-updated");
             $("#payment_info_reservation_total").html("$243.97");
             $("#payment_info_store_credit_row").removeClass("d-none");
             $("#payment_info_remaining_balance").html("$35");
             $("#payment_info_payment_methods").removeClass("d-none");
             $("#no_payment_due_text").addClass("d-none")
             $("#cta_btn_row").removeClass("d-none")
+            $("#balance_value_top").html("$35");
+            $("#balance_value_bottom").html("$35");
         }
     },10);
 });
