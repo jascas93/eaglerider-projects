9febed6e33e7d186a3e47e0c7331532de813e728
jascas93 <jascas_14@hotmail.com>
2022-07-05 14:49:15 -0400

    u

---
+++ jira-issues-tasks/er-3398/js-files/vehicle-reservation-details.js
@@ -0,0 +1,8 @@
+$(window).on("load", function () {
+    setTimeout(removeLoader, 800); //wait x ms
+});
+function removeLoader() {
+    $("#page_loading").fadeOut(400, function () {
+        $("#page_loading").hide();
+    });
+}
