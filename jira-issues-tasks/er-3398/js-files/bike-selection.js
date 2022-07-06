2221d7c511365cb515abf7ae7d82f0c22d2d9551
jascas93 <jascas_14@hotmail.com>
2022-07-06 12:46:25 -0400

    u

--- jira-issues-tasks/er-3398/js-files/bike-selection.js
+++ jira-issues-tasks/er-3398/js-files/bike-selection.js
@@ -100,6 +100,7 @@ $(document).ready(function () {
     $(document).mousemove(function(){
          if($("#model_upgrade_card_link:hover").length != 0){
             $("#model_upgrade_bike_card_img").attr("style", "transform: scale(1.05);");
+            $("#select_model_upgrade_btn").trigger("hover");
         } else{
             $("#model_upgrade_bike_card_img").attr("style", "");
         }
