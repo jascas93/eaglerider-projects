9436cbca4df0f83f636e1b1c2f8136f1a40c6015
jascas93 <jascas_14@hotmail.com>
2022-07-12 16:46:21 -0400

    u

--- jira-issues-tasks/er-3398/js-files/review-and-update.js
+++ jira-issues-tasks/er-3398/js-files/review-and-update.js
@@ -200,5 +200,55 @@ $(document).ready(function () {
             $("#add_extras_btn_1").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-add-extras.html?extended=1");
             $("#add_extras_btn_2").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-add-extras.html?extended=1");
         }
+        else if (window.location.href.indexOf("upsell_added=1") < 1 && window.location.href.indexOf("model_upgraded=1") > 0 && window.location.href.indexOf("shifted=1") > 0) {
+            $("#old_bike_model_name").removeClass("d-none");
+            $("#current_bike_model_img").attr("src", "https://raw.githubusercontent.com/jascas93/eaglerider-projects/main/jira-issues-tasks/er-3398/assets/hd-tri-glide-for-rent.jpeg");
+            $("#current_bike_model_name").html("Harley-Davidson® Tri Glide® Ultra");
+
+            $("#tm_item_1").html("1 Day @ $335 /Day");
+            $("#tm_item_2").html("1 Day @ $335 /Day");
+            $("#tm_price_1").html("$335");
+            $("#tm_price_2").html("$335");
+            $("#discount_price_1").html("-$67");
+            $("#discount_price_2").html("-$67");
+            $("#surcharge_price_1").html("$17.42");
+            $("#surcharge_price_2").html("$17.42");
+            $("#tax_price_1").html("$27.12");
+            $("#tax_price_2").html("$27.12");
+            $("#total_value_1").html("$312.54");
+            $("#paid_value_1").html("($208.97)");
+            $("#balance_value_1").html("$103.57");
+            $("#total_value_2").html("$312.54");
+            $("#paid_value_2").html("($208.97)");
+            $("#balance_value_2").html("$103.57");
+
+            $("#payment_info_reservation_total").html("$312.54");
+            $("#payment_info_store_credit_row").removeClass("d-none");
+            $("#payment_info_membership_credit_row").removeClass("d-none");
+            $("#payment_info_remaining_balance").html("$103.57");
+            $("#payment_info_payment_methods").removeClass("d-none");
+            $("#no_payment_due_text").addClass("d-none");
+            $("#cta_btn_row").removeClass("d-none");
+
+            $("#balance_value_top").html("$103.57");
+            $("#balance_value_bottom").html("$103.57");
+
+            $("#steps_wide_link_details").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-pickup-and-dropoff.html?shifted=1&model_upgraded=1");
+            $("#steps_wide_link_bike").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-bike-selection.html?shifted=1&model_upgraded=1");
+            $("#steps_wide_link_extras").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-add-extras.html?shifted=1&model_upgraded=1");
+            $("#steps_wide_link_review").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html?shifted=1&model_upgraded=1#");
+
+            $("#steps_normal_link_details").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-pickup-and-dropoff.html?shifted=1&model_upgraded=1");
+            $("#steps_normal_link_bike").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-bike-selection.html?shifted=1&model_upgraded=1");
+            $("#steps_normal_link_extras").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-add-extras.html?shifted=1&model_upgraded=1");
+            $("#steps_normal_link_review").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-review-and-update.html?shifted=1&model_upgraded=1#");
+
+            $("#change_model_btn").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-bike-selection.html?shifted=1&model_upgraded=1");
+            $("#change_pickup_btn").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-pickup-and-dropoff.html?shifted=1&model_upgraded=1");
+            $("#change_dropoff_btn").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-pickup-and-dropoff.html?shifted=1&model_upgraded=1");
+
+            $("#add_extras_btn_1").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-add-extras.html?shifted=1&model_upgraded=1");
+            $("#add_extras_btn_2").attr("href", "https://jascas93.github.io/eaglerider-projects/jira-issues-tasks/er-3398/prototypes/modify-reservation-add-extras.html?shifted=1&model_upgraded=1");
+        }
     },10);
 });
