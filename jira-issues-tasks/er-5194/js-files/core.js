d58469c0bfaa79045ea27f9568bb2138aa17bbae
jascas93 <jascas_14@hotmail.com>
2022-10-04 18:42:13 -0400

    u

--- jira-issues-tasks/er-5194/js-files/core.js
+++ jira-issues-tasks/er-5194/js-files/core.js
@@ -1,3 +1,13 @@
+// Page Loading Screen
+$(window).on("load", function () {
+    setTimeout(removeLoader, 800); //wait x ms
+});
+function removeLoader() {
+    $("#page_loading").fadeOut(400, function () {
+        $("#page_loading").hide();
+    });
+}
+// Carousel Sliders
 const slider = document.querySelector(".carousel-inner");
 let isDown = false;
 let startX;
